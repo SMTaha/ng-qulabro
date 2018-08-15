3577e76f9a4116ab89360b420dee6bc660f14cff
--global <adnannice@gmail.com>
2018-08-15 14:54:57 +0500

    feat(header): add dashboard header

--- src/app/dashboard/shared/header/header.component.ts
+++ src/app/dashboard/shared/header/header.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faUserCog } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  faSearch = faSearch;
+  faUserCog = faUserCog;
   constructor() { }
 
   ngOnInit() {
